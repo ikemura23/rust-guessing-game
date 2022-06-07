51ee059cd63b8fe4ab0c2cb678600c62e8a11457
ikemura23 <banbara.work@gmail.com>
2022-06-07 21:09:08 +0900

    コメント追加

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use rand::Rng;
 fn main() {
     println!("数を当ててごらん");
 
+    // 下限値は含むが上限値は含まない、1..100の範囲になる
+    // 同等で 1..=100 という範囲も渡せる
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("秘密の数値は次の通り: {}", secret_number);
